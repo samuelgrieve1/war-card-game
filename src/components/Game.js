2866b743cb5daa9c7d57e17474e2fa6330c02cdb
Sam Grieve <samuel.grieve@icloud.com>
2021-10-04 16:11:54 -0700

    - Made score nicer - Implemented if loadState to prevent errors from clciking too fast - Added catch to fetch that throws an error to user - Added .finally to fetches

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,24 +1,53 @@
+// Import React
 import React, { useState, useEffect } from 'react'
+
+// Get Helpers
+import { getValue } from '../helpers/helpers'
+
+// Get Images
 import cardSpread from '../img/card-spread.png'
 import cardSpreadUpsideDown from '../img/card-spread-upside-down.png'
 import cardSpace from '../img/card-space.png'
-import { getValue } from '../helpers/helpers'
-import spock from '../img/spock.png'
+import logo from '../img/logo.png'
 
+// Initialize Game
 const Game = (props) => {
+
   let compCardCodes = ""
   let userCardCodes = ""
 
   const [deck, setDeck] = useState({})
   const [compCardPlayed, setCompCardPlayed] = useState(null)
-  // const [compCardFetchStarted, setCompCardFetchStarted] = useState(false)
   const [userCardPlayed, setUserCardPlayed] = useState(null)
-  // const [bothCardsPlayed, setBothCardsPlayed] = useState(false)
   const [compCardsRemaining, setCompCardsRemaining] = useState(0)
   const [userCardsRemaining, setUserCardsRemaining] = useState(0)
+  const [loadState, setLoadState] = useState(false)
 
+    // TODO
+    // put second fetches into prior then (It looks like maybe thise has already been done on lines 56 - 92?)
+    // visual loader
+    // Show real number of cards
+    // Use animation to move cards || flip animation
+    // Hide everything until name is entered (cards)
+    // sam vs computer smaller
+    // play button nicer
+    // visual loader: spin circle over cards
+    // Try vue.js
+
+    // DONE
+    // score nicer
+    // if (loadState) return
+    // setLoadState = true
+    // Set load state false
+    // add catch to fetch that throws an error to user
+    // .finally (put load state inside this)
+
+
+  // Start Game
   // Create/shuffle deck
-  useEffect( () => {
+  let startGame = event => {
+    document.getElementById('startGame').classList.add('hidden')
+    document.getElementById('playACard').classList.remove('hidden')
     fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
     .then(response => response.json())
     .then(deck => {
@@ -56,7 +85,6 @@ const Game = (props) => {
         })
       }) 
     })
-
     // Get users name
     if(document.getElementsByClassName("userName")){
       let userNameInput = prompt("What's your name?")
@@ -68,7 +96,13 @@ const Game = (props) => {
         i.innerHTML = userNameInput
       }
     }
-  }, [])
+    // Show Card Spreads
+    let compCardStack = document.getElementById('comp-card-stack')
+    for(let i = 0; i < 26; i++){
+      compCardStack.innerHTML += '<img src={backOfCard}></img>'
+    }
+
+  }
 
   // // Computer plays first card
   // useEffect( () => {
@@ -85,21 +119,31 @@ const Game = (props) => {
 
   // Plays cards
   let playACard = event => {
-    if(deck && deck.deck_id){
+    if(deck && deck.deck_id && !loadState){
+      setLoadState(true)
       // Computer
       fetch(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/pile/compPile/draw/bottom/?count=1`)
       .then(response => response.json())
       .then(compCard => {
         setCompCardPlayed(compCard.cards[0])
         setDeck(compCard)
+        return fetch(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/pile/userPile/draw/bottom/?count=1`)
       })
       // User
-      fetch(`https://deckofcardsapi.com/api/deck/${deck.deck_id}/pile/userPile/draw/bottom/?count=1`)
       .then(response => response.json())
       .then(userCard => {
         setUserCardPlayed(userCard.cards[0])
         setDeck(userCard)
       })
+      .catch(() => {alert("The promise has been broken!")})
+      .finally(
+        setLoadState(false)
+      )
+      // Add/Remove cards from decks
+      let compCardStack = document.getElementById('comp-card-stack')
+      for(let i = 0; i < compCardsRemaining; i++){
+        compCardStack.innerHTML += "Hello "
+      }
     }
   }
 
@@ -114,7 +158,6 @@ const Game = (props) => {
           setCompCardsRemaining(compCardsRemaining - 1)
           setUserCardsRemaining(userCardsRemaining + 1)
         })
-
       } else if(getValue(userCardPlayed.value) < getValue(compCardPlayed.value)){
         setCompCardsRemaining(compCardsRemaining + 1)
         setUserCardsRemaining(userCardsRemaining - 1)
@@ -166,34 +209,43 @@ const Game = (props) => {
   return(
     <div>
       <div className="grid-x">
-
         <div className="cell medium-4 black-bg cell-padding-top lefty">
-          <div className="cell medium-12"><p><img id="ship" src={spock}></img></p><h3>War: Star Trek Edition</h3><br></br></div>
+          <div className="cell medium-12"><p><img id="ship" src={logo}></img></p><br></br></div>
           <div className="cell medium-12">
-            <h1>Computer<br></br>vs<br></br><span className="userName"></span></h1>
-            <br></br>
-            <p><b>Score:</b> &nbsp;&nbsp;&nbsp;&nbsp; Computer: 0 &nbsp;&nbsp;&nbsp;&nbsp; <span className="userName"></span>: 0</p>
+            {/* Comp vs User */}
+            <div className="t-user-vs-comp">
+              <h1>Computer vs. <span className="userName"></span></h1>
+            </div>
             <br></br>
           </div>
           <div className="cell medium-12">
-            <button className="button" id="playACard" onClick={playACard}>Play a card</button>
-            <p>(You can also click on your deck to play a card)</p>
+            <div className="t-score-board">
+            {/* Score */}
+            <div className="t-score-big">
+              <p>Score</p>
+            </div>
+            <div className="t-score-small">
+              <p>Computer: 0<br></br><span className="userName"></span>: 0</p>
+            </div>
+            <br></br>
+            </div>
+          </div>
+          <div className="cell medium-12">
+            <button className="button" id="startGame" onClick={startGame}>Start Game</button>
+            <button className="button hidden" id="playACard" onClick={playACard}>Play a card</button>
           </div>
         </div>
-
         <div className="cell medium-8 table-bg righty">
           <div className="grid-x">
-            
             <div className="cell medium-12 compCards">
               <div className="grid-x">
                 <div className="cell medium-2"></div>
                 <div className="cell medium-10" id="comp-card-stack">
-                  <img src={cardSpreadUpsideDown}></img>
+                  {/* <img src={cardSpreadUpsideDown}></img> */}
                 </div>
                 {/* <div className="cell medium-6 card-stack rotate-left" id="comp-discard-stack"></div> */}
               </div>
             </div>
-            
             <div className="cell medium-12 cardsPlayed">
               <div className="grid-x">
                 <div className="cell medium-3 cardNumbers"></div>
@@ -212,7 +264,6 @@ const Game = (props) => {
                 <div className="cell medium-3"></div>
               </div>
             </div>
-
             <div className="cell medium-12 userCards">
               <div className="grid-x">
                 {/* <div className="cell medium-6 card-stack rotate-left" id="user-discard-stack"></div> */}
@@ -221,15 +272,13 @@ const Game = (props) => {
                 </div>
                 <div className="cell medium-10" id="user-card-stack" onClick={playACard}>
                   {/* <img src={backOfCard}></img> */}
-                  <img src={cardSpread}></img>
+                  {/* <img src={cardSpread}></img> */}
                 </div>
                 <div className="cell medium-2"></div>
               </div>
             </div>
-
           </div>
         </div>
-
       </div>
     </div>
   )
